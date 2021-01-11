8e63df1884befcae4329675b5a92c300a508250e
Vishesh <vishesht27@gmail.com>
2021-01-11 12:58:43 +0530

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main () => runApp(
 MaterialApp(
   title:"Wheather App",
@@ -19,18 +20,47 @@ class _HomeState extends State<Home>{
   Widget build (BuildContext context){
     return Scaffold(
     body:Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment:CrossAxisAlignment.center,
+
       children:<Widget>[
         Container(
           height:MediaQuery.of(context).size.height/3,
           width:MediaQuery.of(context).size.width,
-          color:Colors.red
+          color:Colors.red,
           child : Column(
-            children: [
+            children:<Widget> [
+              Padding(
            padding: EdgeInsets.only(bottom:10.0),
            child:Text(
-            
+            "Currently in Bostom",
+            style: TextStyle(
+              color:Colors.white,
+              fontSize: 14.0,
+              fontWeight: FontWeight.w600
+            ),
            ),
-          ],
+           ),
+           Text(
+             "52\u00B0",
+             style: TextStyle(
+               color: Colors.white,
+               fontSize: 40.0,
+                fontWeight: FontWeight.w600
+             ),
+           ),
+           Padding(
+           padding: EdgeInsets.only(bottom:10.0),
+           child:Text(
+            "Rain",
+            style: TextStyle(
+              color:Colors.white,
+              fontSize: 14.0,
+              fontWeight: FontWeight.w600
+            ),
+           ),
+           ),
+            ],
           ),
         )
       ],
